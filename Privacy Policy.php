6f208064bb40a199a45678b7b50069c6d8e6b8ae
Pooh <poozaza2006@hotmail.com>
2017-12-24 00:19:28 +0700

    dfsd

--- Privacy Policy.php
+++ Privacy Policy.php
@@ -1,3 +1,39 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+<body>
+  <table width="80%" align="center" border="1">
 <?php
-echo "test";
-?>
+$cout = 52;
+$co = 0;
+/////////////////////////////////////
+for ($i=1 ; $i <= 13 ;$i++){
+ echo "<tr>";
+  for ($c=1 ; $c <= 4 ;$c++){
+/////////////////////////////////////////////////
+$datas[$co] = rand(1,$cout);
+$r=0;
+while($r<$co)
+{
+  if ($datas[$co] == $datas[$r])
+  {
+    $datas[$co] = rand(1,$cout);
+    $r = 0;
+  }
+  else{
+    $r++;
+  }
+}
+///////////////////////////////////////////////
+ echo "<td><center>$datas[$co]</center></td>" ;
+ $co++;
+ }
+ echo " </tr> " ;
+ }
+ ?>
+</table>
+</body>
+</html>
