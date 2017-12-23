bf3ecfde754983f152c6fdfd0c37a5eb38819ed2
Pooh <poozaza2006@hotmail.com>
2017-12-24 00:23:12 +0700

    vfdxvx

--- Privacy Policy.php
+++ Privacy Policy.php
@@ -5,34 +5,10 @@
 <title>Untitled Document</title>
 </head>
 <body>
-  <table width="80%" align="center" border="1">
+  
 <?php
-$cout = 52;
-$co = 0;
-/////////////////////////////////////
-for ($i=1 ; $i <= 13 ;$i++){
- echo "<tr>";
-  for ($c=1 ; $c <= 4 ;$c++){
-/////////////////////////////////////////////////
-$datas[$co] = rand(1,$cout);
-$r=0;
-while($r<$co)
-{
-  if ($datas[$co] == $datas[$r])
-  {
-    $datas[$co] = rand(1,$cout);
-    $r = 0;
-  }
-  else{
-    $r++;
-  }
-}
-///////////////////////////////////////////////
- echo "<td><center>$datas[$co]</center></td>" ;
- $co++;
- }
+
  echo " </tr> " ;
- }
  ?>
 </table>
 </body>
